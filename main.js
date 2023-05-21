4a9834d6096f241d7d686972ef3aea6e59bf6749
manav <mnvgpl@gmail.com>
2023-05-22 00:40:20 +0530

    updated and added geolocation feature

--- main.js
+++ main.js
@@ -1,11 +1,19 @@
 const DAYS_OF_THE_WEEK = ['sun', 'mon', 'tue', 'wed', 'thu', 'fri', 'sat'];
+let selectedCityText;
+let selectedCity;
+
 //API_KEY from open weather website..
 const API_KEY = "2885a2c4947ec0377208bcc84e238d2d";
 
+const getCitiesGeoLocation = async (searchText) => {
+    const response = await fetch(`http://api.openweathermap.org/geo/1.0/direct?q=${searchText}&limit=5&appid=${API_KEY}`);
+    return response.json();
+}
+
 //get data from the API....
-const getCurrentWeatherData = async () => {
-    const city = "bilaspur";
-    const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}&units=metric`);
+const getCurrentWeatherData = async ({lat,lon,name: city}) => {
+    const url = lat && lon ? `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric` : `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}&units=metric`;
+    const response = await fetch(url);
     return response.json();
 }
 
@@ -37,13 +45,13 @@ const loadCurrentForecast = ({ name, main: { temp, temp_max, temp_min }, weather
 
 //Loading the hourly forecast
 
-const loadHourlyForecast = ({main:{temp: tempNow},weather:[{icon: iconNow}]}, hourlyForecast) => {
-    const timeFormatter = Intl.DateTimeFormat("en",{
-        hour12:true, hour:"numeric"
+const loadHourlyForecast = ({ main: { temp: tempNow }, weather: [{ icon: iconNow }] }, hourlyForecast) => {
+    const timeFormatter = Intl.DateTimeFormat("en", {
+        hour12: true, hour: "numeric"
     });
     let dataFor12Hours = hourlyForecast.slice(2, 14); // 12 entries
     const hourlyContainer = document.querySelector(".hourly-container");
-    let innerHTMLString = 
+    let innerHTMLString =
         `<article>
         <h3 class="time">Now</h3>
         <img class="icon" src="${createIconUrl(iconNow)}" alt="Not found" />
@@ -94,11 +102,11 @@ const loadFiveDayForecast = (hourlyForecast) => {
     const dayWiseForecast = calculateDayWiseForecast(hourlyForecast);
     const container = document.querySelector('.five-day-forecast-container');
     let dayWiseInfo = "";
-    Array.from(dayWiseForecast).map(([day, { temp_max, temp_min, icon }], index)=>{
+    Array.from(dayWiseForecast).map(([day, { temp_max, temp_min, icon }], index) => {
 
-        if(index < 5){
+        if (index < 5) {
             dayWiseInfo += `<article class="day-wise-forecast">
-                        <h3 class="day">${index === 0? "Today": day}</h3>
+                        <h3 class="day">${index === 0 ? "Today" : day}</h3>
                         <img class="icon" src="${createIconUrl(icon)}" alt="Not Found" />
                         <p class="min-temp">${formatTemprature(temp_min)}</p>
                         <p class="max-temp">${formatTemprature(temp_max)}</p>
@@ -118,12 +126,79 @@ const loadHumidity = ({ main: { humidity } }) => {
     container.querySelector(".humidity-value").textContent = `${humidity} %`;
 }
 
-document.addEventListener("DOMContentLoaded", async () => {
-    const currentWeather = await getCurrentWeatherData();
+const loadData = async () => {
+    const currentWeather = await getCurrentWeatherData(selectedCity);
+    console.log(currentWeather);
     loadCurrentForecast(currentWeather);
     const hourlyForecast = await getHourlyForecast(currentWeather);
     loadHourlyForecast(currentWeather, hourlyForecast);
     loadFiveDayForecast(hourlyForecast)
     loadFeelsLike(currentWeather);
     loadHumidity(currentWeather);
+}
+
+loadForecastUsingGeoLocation = () => {
+    navigator.geolocation.getCurrentPosition(({coords}) =>{
+        const {latitude: lat, longitude: lon} = coords;
+        selectedCity = {lat,lon};
+        loadData();
+    }, error => console.log(error))
+}
+//Search input debouncing to get delay in fetching
+
+function debounce(func) {
+    let timer;
+    return (...args) => {
+        clearTimeout(timer); // It clears existing timout
+        // Create a new time till the user typing....
+        timer = setTimeout(() => {
+            console.log("debounce");
+            func.apply(this, args);
+        }, 500);
+    }
+}
+
+//display the options on the screen
+const onSearchChange = async (event) => {
+    let { value } = event.target;
+    if (!value) {
+        selectedCity = null;
+        selectedCityText = ""; 
+    }
+    if (value && (selectedCityText !== value)) {
+        const listOfCities = await getCitiesGeoLocation(value);
+        let option = "";
+        for (let { lat, lon, name, state, country } of listOfCities) {
+            let cityDetails = JSON.stringify({ lat, lon, name });
+            option += `<option data-city-details='${cityDetails}' value="${name}, ${state}, ${country}"></option>`;
+        }
+        document.querySelector("#cities").innerHTML = option;
+        console.log(listOfCities);
+    }
+    
+}
+
+//selection of the option
+const handleCitySelection = (event) => {
+    console.log("Selection done");
+    selectedCityText = event.target.value;
+    let options = document.querySelectorAll("#cities > option");
+    console.log(options);
+    if (options?.length) {
+        let selectedOption = Array.from(options).find(opt => opt.value === selectedCityText);
+        selectedCity = JSON.parse(selectedOption.getAttribute("data-city-details"));
+        console.log({ selectedCity });
+        loadData();
+    }
+}
+
+const debounceSearch = debounce((event) => onSearchChange(event));
+
+document.addEventListener("DOMContentLoaded", async () => {
+    loadForecastUsingGeoLocation();
+    const searchInput = document.querySelector("#search");
+    searchInput.addEventListener("input", debounceSearch);
+    searchInput.addEventListener("change", handleCitySelection);
+
+    
 })
